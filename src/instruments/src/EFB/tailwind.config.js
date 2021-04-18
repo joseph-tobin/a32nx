318bda8e3e9a301afded36d69d08f7430b9f2953
benjozork <benjamin.dupont.984@gmail.com>
2021-04-18 03:42:17 -0400

    perf(efb): enable tailwind purging

--- src/instruments/src/EFB/tailwind.config.js
+++ src/instruments/src/EFB/tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         ],
     },
     darkMode: false, // or 'media' or 'class'
-    theme: { extend: { height: () => ({ efb: '50rem', 'efb-nav': '45.75rem' }) } },
+    theme: { extend: { height: () => ({ 'efb': '50rem', 'efb-nav': '45.75rem' }) } },
     variants: { extend: {} },
     // eslint-disable-next-line global-require
     plugins: [require('@flybywiresim/tailwind-config')],
